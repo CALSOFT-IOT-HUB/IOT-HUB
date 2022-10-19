bd6fdb471a52d859f742a57c7ea3de8e004f16ef
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-10-19 13:08:39 +0530

    React Boiler plate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+import MainBody from './Components/MainBody'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='App'>
+        <div className='App-sidebar'>
+          <Sidebar />
+        </div>
+        <div className='App-main'>
+          <MainBody/>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/MainBody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainBody = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainBody
